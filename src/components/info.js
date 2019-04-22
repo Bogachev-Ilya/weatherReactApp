b51a650ac74b4f501228d25e890edb7367ab8aa8
Ilia Bogachev <ilia_bogachev@epam.com>
2019-04-22 16:05:43 +0300

    first commit

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Info extends React.Component{
+    render(){
+        return(
+            <div>
+            <h2>Weather info</h2>
+            <p>Know you city's weather</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
